447f0ca789577199037259ad02ea3829223afd84
Leo Cheung <lappangcheung@gmail.com>
2024-11-26 01:21:23 -0500

    Setting up the story

---
+++ src/components/Story.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Story = () => {
+  return (
+    <section id="story"
+             className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p>The multiversal ip world</p>
+      </div>
+    </section>
+  )
+}
+export default Story
